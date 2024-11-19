8bb8f9333fcc9071674cd3f9cb97c4617e71a0f3
Colson Arthur <arthur.colson@etu.iut-tlse3.fr>
2024-11-19 19:07:43 +0100

    omfg i see my snake

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 import { Player } from './modules/Player.js';
-import { initializeCanvas,drawGrid } from './modules/displayGrid.js';
+import { draw,drawBlankGrid,cellSize } from './modules/displayGame.js';
 
+let canvas;
+let context;
 const cooling = 0.012;
 const size = 750;
 let idInterval;
 
-/*
+// Fonction pour redimensionner le canvas
+function resizeCanvas() {
+    // const size = Math.min(window.innerWidth, window.innerHeight);
+    canvas.width = size ;  
+    canvas.height = size ;
+    context.fillStyle = "lightblue";
+    context.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+// Fonction pour initialiser le canvas et le contexte
+function initializeCanvas(canvasElementId) {
+    canvas = document.getElementById(canvasElementId);
+    context = canvas.getContext('2d');
+}
+
 function startGame(){
+    drawBlankGrid();
     let player = new Player();
     player.initialiseFruit();
     return player;
@@ -14,14 +31,14 @@ function startGame(){
 
 
 function midGame(player){
+    draw(player);
     if(player.nextCellIsLoose()){
         clearInterval(idInterval);
     }
     player.moove();
     player.displayGame();
-    }
+}
 
-function endGame(){return;}
 
 function mainGame(){
     let player = startGame();
@@ -34,42 +51,14 @@ function mainGame(){
         console.log("Tu pues tes morts");
     }
 }
-    
-
-mainGame();
-*/
-
-// function draw(){
-//     var canvas = document.getElementById("board");
-//     var ctx = canvas.getContext("2d");
-//     ctx.fillRect()
-//     ctx.canvas.height = heightWindow;
-// }
-
-
-// liste du snake
-// fonction drawHead
-// fonction drawBody
-// ATTENTION VERS OU LE BODY OU LA TETE REGARDENT 
-// fonction drawFruit
-// fonction drawBlankGrid
-
-
-const canvas = document.getElementById('board');
-const context = canvas.getContext('2d');
-initializeCanvas('board');
-
-// Fonction pour redimensionner le canvas
-function resizeCanvas() {
-    //const size = Math.min(window.innerWidth, window.innerHeight);
-    canvas.width = size ;  
-    canvas.height = size ;
-    context.fillStyle = "lightblue";
-    context.fillRect(0, 0, canvas.width, canvas.height);
-}
-
+initializeCanvas('board'); // Réinitialiser le canvas et le contexte
 resizeCanvas();
-drawGrid();
 
 // Réajuster le canvas à chaque changement de taille de la fenêtre
-window.addEventListener('resize', resizeCanvas);
+window.addEventListener('resize', () => {
+    resizeCanvas();
+});  
+mainGame();
+
+export { canvas, context };
+
